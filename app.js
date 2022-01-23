c051eee2b144ffe82bb866e800d7b0bc088ed14a
Miranda <mirjade2008@yahoo.com>
2022-01-23 17:54:37 -0500

    updated hw

--- app.js
+++ app.js
@@ -7,33 +7,17 @@ const morgan = require('morgan');
 app.set("view engine", "ejs");
 
 app.listen(3000);
-app.use(morgan(dev));
-app.use(express.static('public'));
 
-// app.use((req, res, next) => {
-//     console.log('new request made:');
-//     console.log('host: ', req.hostname);
-//     console.log('path: ', req.path);
-//     console.log('method: ', req.method);
-//     next();
-//   });
-  
-//   app.use((req, res, next) => {
-//     console.log('new request made:');
-//     console.log('host: ', req.hostname);
-//     console.log('path: ', req.path);
-//     console.log('method: ', req.method);
-//     next();
-//   });
-  
 
-const blogs = [
+
+  app.get("/", (req, res) => {
+    const blogs = [
         {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
         {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
         {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
       ];
-    
-
+    res.render("index", { title: 'Home', blogs});
+    });
 
 app.get("/", (req, res) => {
     // res.send("<p>home page</p>");
@@ -46,9 +30,7 @@ app.get("/about", (req, res) => {
     res.render('about', { title: 'About'});
 });
 
-// app.get('/about-us', (req, res) => {
-//    // res.redirect('/about');
-// });
+
 
 
 app.get('/blogs/create', (req, res) => {
